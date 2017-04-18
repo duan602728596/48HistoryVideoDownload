507f13a3cdfb9af3d48a29c27d8514b60db77cbb
duanhaochen <duanhaochen@126.com>
2017-04-18 21:09:59 +0800

    在使用新地址的mp4的同时，对旧地址的ts流下载进行了兼容

--- download.py
+++ download.py
@@ -1,7 +1,9 @@
 # coding=utf8
 # 下载并保存视频流
 import datetime
+import re
 import win32api
+from get import getTs
 
 
 # 中文转英文
@@ -13,6 +15,25 @@ def cn2en(txt):
     elif txt == u'流畅':
         return 'liuchang'
 
+# 对地址进行解析
+# invedio：m3u8地址
+PATTERN_URL1 = re.compile(r'http://ts\.snh48\.com/\d+/', re.I)  # 旧网址
+PATTERN_URL2 = re.compile(r'http://ts\.snh48\.com/\d+/[^\.]+/', re.I)  # 提取网址
+def getTsUrl(m3u8):
+    s = None
+    r1 = PATTERN_URL1.findall(m3u8)
+    r2 = PATTERN_URL2.findall(m3u8)
+
+    if len(r2) != 0:
+        s = r2[0]
+    elif len(r1) != 0:
+        s = r1[0]
+    else:
+        pass
+
+    return s
+
+
 # 调用ffmpeg.exe
 # Qt_Infor 信息模块
 # invedio: m3u8的地址
@@ -20,10 +41,49 @@ def cn2en(txt):
 # address: 星梦剧院地址
 # id: 视频id
 # pinzhi: 视频的质量
-def download(Qt_Infor, m3u8, address, id, pinzhi, title):
+stopDownload = False
+def download(Qt_Infor, invedio, m3u8, tsUrl, address, id, pinzhi, title):
     now = datetime.datetime.now()
-    fl = address + '_' + id + '_' + cn2en(pinzhi) + '_' + now.strftime('%Y-%m-%d-%H-%M-%S') + '.mp4' # 使用时间戳
-    arg = '-i ' + m3u8 + ' -acodec copy -vcodec copy -f mp4 output/' + fl
-    Qt_Infor.setText(u'下载视频：' + title + '\n' +
-                     u'输出文件：' + fl)
-    win32api.ShellExecute(0, 'open', 'ffmpeg.exe', unicode(arg), '', True)
+    host = getTsUrl(m3u8)
+    fl = address + '_' + id + '_' + cn2en(pinzhi) + '_' + now.strftime('%Y-%m-%d-%H-%M-%S')  # 使用时间戳
+
+    # 新的url使用ffmpeg进行下载
+    if host == None:
+        arg = '-i ' + m3u8 + ' -acodec copy -vcodec copy -f mp4 output/' + fl + '.mp4'
+        Qt_Infor.setText(u'下载视频：' + title + '\n' +
+                         u'输出文件：' + fl + '.mp4')
+        win32api.ShellExecute(0, 'open', 'ffmpeg.exe', unicode(arg), '', True)
+
+    # 旧的url使用传统的get方法下载
+    else:
+        headers = {
+            'Referer': invedio,
+            'X-Requested-With': 'ShockwaveFlash/24.0.0.221',
+        }
+        file = open('output/' + fl + '.ts', 'ab')
+        i = 0
+        length = len(tsUrl)
+        while i < length:
+            global stopDownload
+            if stopDownload == False:
+                u = host + tsUrl[i] + '.ts'
+                buffer = getTs(u, headers)
+
+                code = None
+                if buffer != 404:
+                    file.write(buffer)
+                    code = '200'
+                else:
+                    code = '404'
+
+                Qt_Infor.setText(u'写入文件：' + fl + '.ts' + '\n' +
+                                 str(i + 1) + '/' + str(length) + ' ' + host + tsUrl[i] + '.ts ' + code)
+                i += 1
+
+                if i == length:
+                    Qt_Infor.setText(u'下载完成：' + fl + '.ts' + '\n' +
+                                     str(i + 1) + '/' + str(length) + ' ' + host + tsUrl[i] + '.ts')
+            else:
+                Qt_Infor.setText(u'停止下载：' + fl + '.ts' + '\n' +
+                                 str(i + 1) + '/' + str(length) + ' ' + host + tsUrl[i] + '.ts')
+                break
--- get.py
+++ get.py
@@ -9,3 +9,19 @@ def get(url):
     request = urllib2.Request(url)
     response = urllib2.urlopen(request)
     return response.read()
+
+
+# ts请求
+# url：请求的地址
+# headers：请求头
+def getTs(url, headers):
+    request = urllib2.Request(url, headers = headers)
+    r = None
+
+    try:
+        response = urllib2.urlopen(request)
+        r = response.read()
+    except:
+        r = 404
+
+    return r
